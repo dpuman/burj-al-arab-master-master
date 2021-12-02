8fcb0550575ef22cab958bc78405d256921bb06e
dpuman <dpumail.in@gmail.com>
2021-12-02 11:25:43 +0600

    initial and compleation project

---
+++ src/components/Login/firebase.configaration.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQcgfRY-ma_z00ktrSJyKRL-IAyrzh2gY",
+    authDomain: "burj-al-arab-dipu.firebaseapp.com",
+    projectId: "burj-al-arab-dipu",
+    storageBucket: "burj-al-arab-dipu.appspot.com",
+    messagingSenderId: "948112667248",
+    appId: "1:948112667248:web:86d6cb2296c026c918bab5"
+};
